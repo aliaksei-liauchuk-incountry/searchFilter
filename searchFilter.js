204b68d4a35e416c322d57b07906410adfb02600
ViktorPenzar <penzar@mail.ru>
2022-07-30 22:40:46 +0300

    Working Search Filter Project 1

--- searchFilter.js
+++ searchFilter.js
@@ -1,40 +1,58 @@
-import { LightningElement, wire } from 'lwc';
+import {LightningElement, track, wire} from 'lwc';
 import searchContacts from '@salesforce/apex/ContactController.searchContacts';
-import FIRSTNAME_FIELD from '@salesforce/schema/Contact.FirstName';
-import LASTNAME_FIELD from '@salesforce/schema/Contact.LastName';
-import EMAIL_FIELD from '@salesforce/schema/Contact.Email';
-import PHONE_FIELD from '@salesforce/schema/Contact.MobilePhone';
-import DATE_FIELD from '@salesforce/schema/Contact.CreatedDate';
-import ACCOUNTNAME_FIELD from '@salesforce/schema/Account.Name';
-const COLUMNS = [
-    { label: 'First Name', fieldName: FIRSTNAME_FIELD.fieldApiName, type: 'text' },
-    { label: 'Last Name', fieldName: LASTNAME_FIELD.fieldApiName, type: 'text' },
-    { label: 'Email', fieldName: EMAIL_FIELD.fieldApiName, type: 'email' },
-    { label: 'Account Name', fieldName: ACCOUNTNAME_FIELD.fieldApiName, type: 'text' },
-    { label: 'Mobile Phone', fieldName: PHONE_FIELD.fieldApiName, type: 'phone' },
-    { label: 'Created Date', fieldName: DATE_FIELD.fieldApiName, type: 'date', typeAttributes:{
-        year: "numeric",
-        month: "2-digit",
-        day: "2-digit",
-        hour: "2-digit",
-        minute: "2-digit",
-        hour12: "true"
-        } 
+const columns = [
+    {label: 'First Name', fieldName: 'FirstName', type: 'text'}, 
+    {label: 'Last Name', fieldName: 'LastName', type: 'text'}, 
+    {label: 'Email', fieldName: 'Email', type: 'email'},  
+    {label: 'Mobile Phone', fieldName: 'MobilePhone', type: 'phone'}, 
+    {label: 'Account Name', fieldName: 'AccountName', type: 'text'}, 
+    {label: 'CreatedDate', fieldName: 'CreatedDate', type: 'date',
+        typeAttributes:{
+            year: "numeric",
+            month: "2-digit",
+            day: "2-digit",
+            hour: "2-digit",
+            minute: "2-digit",
+            hour12: "true"
+        }
     }
 ];
 export default class ContactList extends LightningElement {
-	ampm = true;
-    columns = COLUMNS;
+    ampm = true;
     searchValue = '';
     searchKey = '';
-   
+    @track data = [];
+    @track columns = columns;
+
     @wire(searchContacts, {searchKey: '$searchKey'})
-	contacts;
-	
-	handleWordChange(event) {
+    con({error, data}) {
+        if(data) {
+            let currentData = [];
+            data.forEach((row) => {
+                let rowData = {};
+                rowData.FirstName = row.FirstName;
+                rowData.LastName = row.LastName;
+                rowData.Email = row.Email;
+                rowData.MobilePhone = row.MobilePhone;
+                rowData.CreatedDate = row.CreatedDate;
+
+                if (row.Account) {
+                    rowData.AccountName = row.Account.Name;
+                }
+                currentData.push(rowData);
+            });
+            this.data = currentData;
+        }
+        else if(error) {
+            window.console.log(error);
+        }
+    }
+
+    handleWordChange(event) {
 		const searchValue = event.target.value;
 			this.searchValue = searchValue;
 	}
+
     handleSearchKeyword (event) {
             const searchKey = event.target.value;
 			this.searchKey = searchKey
