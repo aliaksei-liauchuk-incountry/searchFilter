c01f039c172569a78afc2e83b2a4f4217c363778
ViktorPenzar <penzar@mail.ru>
2022-08-15 16:50:36 +0300

    Search Filter + project 2

--- lwc/searchFilter/searchFilter.js
+++ lwc/searchFilter/searchFilter.js
@@ -1,25 +1,38 @@
-import {LightningElement, track, wire} from 'lwc';
-import searchContacts from '@salesforce/apex/ContactController.searchContacts';
+import {LightningElement, api, wire} from 'lwc';
+import searchContacts from '@salesforce/apex/SearchContactController.searchContacts';
+import { NavigationMixin } from 'lightning/navigation'
+import { refreshApex } from '@salesforce/apex';
+
 const columns = [
 	{label: 'First Name', fieldName: 'FirstName', type: 'text'}, 
 	{label: 'Last Name', fieldName: 'LastName', type: 'text'}, 
 	{label: 'Email', fieldName: 'Email', type: 'email'},  
 	{label: 'Mobile Phone', fieldName: 'MobilePhone', type: 'phone'}, 
 	{label: 'Account Name', fieldName: 'AccountLink', type: 'url', typeAttributes: { label: { fieldName: 'AccountName' }, target: '__blank'}}, 
-	{label: 'CreatedDate', fieldName: 'CreatedDate', type: 'date', typeAttributes:{year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', hour12: 'true'}}
+	{label: 'CreatedDate', fieldName: 'CreatedDate', type: 'date', typeAttributes:{year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', hour12: 'true'}},
+	{label: 'Action', fieldName: 'delete', type: 'button', typeAttributes: {label: 'Delete', name: 'delete', variant: 'destructive-text'}, cellAttributes: { alignment: 'right' }},
+	
 ];
-export default class ContactList extends LightningElement {
+export default class ContactList extends NavigationMixin (LightningElement) {
 	searchValue = '';
 	searchKey = '';
-	data = [];
+	data;
+	error;
 	columns = columns;
-
-	@wire(searchContacts, {searchKey: '$searchKey'})
-	wiredContacts({data}) {
-		if(data) {
+	newContactModal = false;
+	delContactModal = false;
+	recordId;
+	firstName;
+	lastName;
+	refreshTable;
+	
+	@wire(searchContacts, {searchKey: '$searchKey'}) wired(result) {
+		this.refreshTable = result;
+		if(result.data) {
 			let currentData = [];
-			data.forEach((row) => {
+			result.data.forEach((row) => {
 				let rowData = {};
+				rowData.Id = row.Id
 				rowData.FirstName = row.FirstName;
 				rowData.LastName = row.LastName;
 				rowData.Email = row.Email;
@@ -43,6 +56,28 @@ export default class ContactList extends LightningElement {
 
 	handleSearchKeyword (event) {
 		const searchKey = event.target.value;
-		this.searchKey = searchKey
+		this.searchKey = searchKey;
+	}
+
+	newContact() {
+		this.newContactModal = true;
+	}
+
+	delContact(event) {
+		this.recordId = event.detail.row.Id;
+		this.firstName = event.detail.row.FirstName;
+		this.lastName = event.detail.row.LastName;
+		this.delContactModal = true;
+	}
+	
+	haldleRefresh() {
+		this.newContactModal = false;
+		this.delContactModal = false;
+		return refreshApex(this.refreshTable);
+	}
+
+	haldleClose() {
+		this.newContactModal = false;
+		this.delContactModal = false;
 	}
 }
